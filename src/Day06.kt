5fb112a4f9fca82b8fefcfeff23917f70decc054
Jacob Botuck <jbotuck@gmail.com>
2023-12-07 14:43:22 -0600

    day 6 alternative solutions

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,7 +1,28 @@
+import kotlin.time.measureTime
+
 fun main() {
     val input = readInput("Day06")
-    part1(input).println()
-    part2(input).println()
+//    part1(input).println()
+//    part2(input).println()
+    val race = input
+        .map { line -> line.filter { it.isDigit() }.toLong() }
+        .let { Race(it.first(), it.last()) }
+    measureTime {
+        race
+            .numberOfWaysToWin()
+            .also { println(it) }
+    }.let { println("brute force done in $it") }
+    measureTime {
+        race
+            .numberOfWaysToWinBs()
+            .also { println(it) }
+    }.let { println("binary search done in $it") }
+    measureTime {
+        race
+            .numberOfWaysToWinQuadratic()
+            .also { println(it) }
+    }.let { println("quadratic done in $it") }
+
 }
 
 private data class Race(val time: Long, val record: Long) {
@@ -9,7 +30,36 @@ private data class Race(val time: Long, val record: Long) {
         return 1L.rangeUntil(time).count { distance(it) > record }
     }
 
+    fun numberOfWaysToWinQuadratic(): Int {
+        //(time - buttonLength) * buttonLength > record
+        //buttonLength^2 - time*buttonLength + record < 0
+        measureTime {
+            val b = -time
+            val c = record
+            val discriminant = b * b - 4 * c
+            val positiveRoot = kotlin.math.sqrt(discriminant.toDouble())
+            val negativeRoot = -positiveRoot
+            val lowerBound = kotlin.math.ceil((negativeRoot - b) / 2).toLong()
+            val upperBound = ((positiveRoot - b) / 2).toLong()
+            return (upperBound.inc() - lowerBound).toInt()
+        }
+    }
+
     private fun distance(buttonLength: Long) = (time - buttonLength) * buttonLength
+    fun numberOfWaysToWinBs(): Long {
+        var r = time / 2
+        var l = 0L
+        fun ok(buttonLength: Long) = distance(buttonLength) > record
+        while(r - l > 1){
+            val m = (l + r) / 2
+            if (ok(m)){
+                r = m
+            } else {
+                l = m
+            }
+        }
+        return time.dec() - (r.dec() * 2)
+    }
 }
 
 
@@ -26,9 +76,8 @@ private fun part1(input: List<String>): Int {
 }
 
 private fun part2(input: List<String>) = input
-    .map { line ->
-        line.filter { it.isDigit() }.toLong()
-    }.let { Race(it.first(), it.last()) }
-    .numberOfWaysToWin()
+    .map { line -> line.filter { it.isDigit() }.toLong() }
+    .let { Race(it.first(), it.last()) }
+    .numberOfWaysToWinQuadratic()
 
 
